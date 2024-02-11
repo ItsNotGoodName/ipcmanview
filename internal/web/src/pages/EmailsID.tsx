68ff508a35d5f1ed43317ff4aa4dbb64ce0c7dfd
ItsNotGoodName <gurnaindeol@gmail.com>
2024-02-10 19:13:22 -0800

    show attachments

--- internal/web/src/pages/EmailsID.tsx
+++ internal/web/src/pages/EmailsID.tsx
@@ -5,7 +5,7 @@ import { Shared } from "~/components/Shared"
 import { formatDate, parseDate } from "~/lib/utils"
 import { buttonVariants } from "~/ui/Button"
 import { CardRoot } from "~/ui/Card"
-import { RiArrowsArrowLeftLine } from "solid-icons/ri"
+import { RiArrowsArrowLeftLine, RiDeviceHardDrive2Line, RiMediaImageLine, RiSystemDownloadLine } from "solid-icons/ri"
 import { LayoutNormal } from "~/ui/Layout"
 import { TabsContent, TabsList, TabsRoot, TabsTrigger } from "~/ui/Tabs"
 import { BreadcrumbsItem, BreadcrumbsLink, BreadcrumbsList, BreadcrumbsRoot, BreadcrumbsSeparator } from "~/ui/Breadcrumbs"
@@ -16,10 +16,13 @@ import { Skeleton } from "~/ui/Skeleton"
 import { PageError } from "~/ui/Page"
 import { Badge } from "~/ui/Badge"
 import { Seperator } from "~/ui/Seperator"
+import { Image } from "@kobalte/core"
+import { TooltipContent, TooltipRoot, TooltipTrigger } from "~/ui/Tooltip"
 
 export function EmailsID({ params }: any) {
   const [searchParams, setSearchParams] = useSearchParams()
   const data = createAsync(() => getEmailsIDPage(BigInt(params.id)))
+  const query = () => searchParams.tab ? "?tab=" + searchParams.tab : ""
   const backPage = () => Math.ceil(Number(data()?.emailSeen) / 10) || 1
 
   return (
@@ -54,10 +57,10 @@ export function EmailsID({ params }: any) {
             <div class="flex items-center gap-2">
               <div>{data()?.emailSeen.toString()} of {data()?.emailCount.toString()}</div>
               <Crud.PageButtonsLinks
-                previousPage={`/emails/${data()?.previousEmailId}`}
+                previousPage={`/emails/${data()?.previousEmailId}${query()}`}
                 previousPageDisabled={data()?.previousEmailId == data()?.id}
                 nextPageDisabled={data()?.nextEmailId == data()?.id}
-                nextPage={`/emails/${data()?.nextEmailId}`}
+                nextPage={`/emails/${data()?.nextEmailId}${query()}`}
               />
             </div>
           </div>
@@ -75,7 +78,7 @@ export function EmailsID({ params }: any) {
                   </tr>
                   <tr>
                     <th class="px-2">To</th>
-                    <td class="px-2 flex gap-2">
+                    <td class="flex gap-2 px-2">
                       <For each={data()?.to}>
                         {v => <Badge>{v}</Badge>}
                       </For>
@@ -97,7 +100,7 @@ export function EmailsID({ params }: any) {
             <div class="overflow-x-auto">
               <TabsList>
                 <TabsTrigger value="text" >Text</TabsTrigger>
-                <TabsTrigger value="attachments" class="flex gap-2 items-center">
+                <TabsTrigger value="attachments" class="flex items-center gap-2">
                   Attachments
                   <Show when={data()?.attachments.length || 0 > 0}>
                     <Badge>{data()?.attachments.length}</Badge>
@@ -108,15 +111,34 @@ export function EmailsID({ params }: any) {
             <TabsContent value="text">
               <pre class="overflow-x-auto">{data()?.text}</pre>
             </TabsContent>
-            <TabsContent value="attachments">
+            <TabsContent value="attachments" class="flex flex-wrap gap-4">
               <For each={data()?.attachments}>
                 {v => (
-                  <div>
-                    <div>{v.id.toString()}</div>
-                    <div>{v.name}</div>
-                    <a href={v.url}>{v.url}</a>
-                    <div>{Humanize.fileSize(Number(v.size))}</div>
+                  <div class="sm:max-w-48 flex w-full flex-col rounded-b border">
+                    <Image.Root class="mx-auto max-h-48 w-full">
+                      <Image.Img src={v.thumbnailUrl} class="h-full w-full object-contain" />
+                      <Image.Fallback>
+                        <RiMediaImageLine class="h-full w-full object-contain" />
+                      </Image.Fallback>
+                    </Image.Root>
                     <Seperator />
+                    <div class="p-2">
+                      <div>
+                        <TooltipRoot>
+                          <TooltipTrigger class="w-full truncate">{v.name}</TooltipTrigger>
+                          <TooltipContent>{v.name}</TooltipContent>
+                        </TooltipRoot>
+                      </div>
+                      <div class="flex items-center justify-between gap-2">
+                        <div title="Size" class="flex items-center gap-1">
+                          <RiDeviceHardDrive2Line class="h-5 w-5" />
+                          {Humanize.fileSize(Number(v.size))}
+                        </div>
+                        <a href={v.url} target="_blank" title="Download">
+                          <RiSystemDownloadLine class="h-5 w-5" />
+                        </a>
+                      </div>
+                    </div>
                   </div>
                 )}
               </For>
